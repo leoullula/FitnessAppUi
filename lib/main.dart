247ac36c0cdfae2fc56ead5f9de7f08139b18519
Apoorva Verma <47896012+vapoorva@users.noreply.github.com>
2019-08-09 12:17:09 +0530

    updates Main.dart
    to see the graph bar change the line 10 in main .dart replace MyApp() to ShowGraph() and to see the fitness app home page do vice versa

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'curve.dart';
-void main() => runApp(ShowGraph());
+void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
