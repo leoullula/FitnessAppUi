7129ec97a2cbaba429311c4780abf5dfcba33f54
Apoorva Verma <47896012+vapoorva@users.noreply.github.com>
2019-09-17 12:31:51 +0530

    Updates Main dart2

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'curve.dart';
-void main() => runApp(MyApp());
+void main() => runApp(ShowGraph());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
