67eaa03a4f09beaf857cbca79f3256409e48bf07
Apoorva Verma <47896012+vapoorva@users.noreply.github.com>
2019-09-17 12:14:48 +0530

    Updated colors.dart

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -5,6 +5,9 @@ Color F1 = Colors.black;
 Color F2 = Colors.black.withOpacity(0.5);
 Color F3 = Colors.black.withOpacity(0.2);
 
+Color greyColor = Color(0xFFE6E6E6);
+Color darkGreyColor = Color(0xFFB8B2CB);
+
 final appTheme = ThemeData(
   primarySwatch: Colors.blue
 );
